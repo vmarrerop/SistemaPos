6714ae8406ea18c681f17f3309a3e4e76a7988f1
vmarrerop <77476818+vmarrerop@users.noreply.github.com>
2023-10-15 03:42:49 -0500

    Add files via upload

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import './Header.scss';
+import '../../App.scss'
+import { LuMenuSquare } from 'react-icons/lu';
+import { RiAccountCircleLine } from 'react-icons/ri';
+import { AiFillHome } from 'react-icons/ai';
+import { BiHomeAlt2 } from 'react-icons/bi';
+import Modal from 'react-modal';
+
+export function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="header">
+      <div className="left-content">
+        <button className="options-button">
+          <Link to="/Services">
+            < BiHomeAlt2 className='icon' />
+          </Link>
+        </button>
+      </div>
+
+      <div className="modal-button center-content md:hidden">
+        <button onClick={openModal} className="options-button">
+          <LuMenuSquare className="icon" />
+        </button>
+      </div>
+
+      <div className="center-content hidden md:flex justify-center">
+        <Link to="/Inventario" className="icon-container">
+          <p className="icon-label1">Inventario</p>
+        </Link>
+        <div className="icon-container">
+          <p className="icon-label2">Nómina</p>
+        </div>
+        <div className="icon-container">
+        <Link to="/Facturas" className="icon-container">
+          <p className="icon-label3">Registros</p>
+          </Link>
+        </div>
+        <div className="icon-container">
+          <p className="icon-label4">Analítica</p>
+        </div>
+      </div>
+
+      <div className="right-content">
+        <Link to="/">
+          <RiAccountCircleLine className="icon" />
+        </Link>
+      </div>
+
+      {/* Ventana modal */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        className="modal-content"
+        overlayClassName="modal-overlay"
+        ariaHideApp={false}
+      >
+        <Link to="/Services" className="icon-containerModal">
+          <AiFillHome className="icon" />
+        </Link>
+
+        <Link to="/Inventario" className="icon-containerModal">
+          <p className="icon-label1">Inventario</p>
+        </Link>
+
+        <div className="icon-containerModal">
+          <p className="icon-label2">Nómina</p>
+        </div>
+
+        
+        <Link to="/Facturas" className="icon-containerModal">
+          <p className="icon-label3">Facturas</p>
+        </Link>
+        
+        <div className="icon-containerModal">
+          <p className="icon-label4">Analítica</p>
+        </div>
+        <div className="close-container">
+          <button onClick={closeModal} className="close">
+            Cerrar
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+}
