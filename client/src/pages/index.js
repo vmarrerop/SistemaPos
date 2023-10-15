1192eb9a20acf9a6ed302946a913ed956b433180
vmarrerop <77476818+vmarrerop@users.noreply.github.com>
2023-10-15 02:01:00 -0500

    Delete client directory

--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { HomePage } from "./Services/HomePage";
-export { NotFoundPage } from "./NotFoundPage";
-export { LoginView } from "./LoginView/LoginView"
-export { Inventario } from './Inventario/Inventario'
-export { Facturas } from "./Facturas/Facturas"
