f480125048b6bbd5173c265915fbda33faff6082
vmarrerop <vmarrerop@gmail.com>
2023-11-11 23:13:51 -0500

    Add 3er cambio Login

--- client/src/pages/LoginView/LoginView.js
+++ client/src/pages/LoginView/LoginView.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.scss';
 import { LuMonitorSmartphone } from 'react-icons/lu'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 export function LoginView () {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, // Ajusta la velocidad de reproducción según tus necesidades
+    cssEase: 'linear',
+    arrows: false,
+  };
+
   return (
     <>
   <div className="main-container min-h-screen flex flex-col justify-center items-center">
@@ -24,16 +40,32 @@ export function LoginView () {
       INGRESAR
     </Link>
     <hr></hr>
-    <div className='message-capsule'>
+
+    
+
+    <div className='message-capsule2'>
       <div className='seccion-izquierda w-2/3 flex flex-col justify-center items-center'>
-        <p className='message2'>Tu tienda en cualquier dispositivo</p>
+        <Slider className="slider" {...settings}>
+          <div>
+            <h1>Facturación</h1>
+          </div>
+          <div>
+            <h1>Inventario</h1>
+          </div>
+          <div>
+            <h1>Nómina</h1>
+          </div>
+          <div>
+            <h1>Analítica</h1>
+          </div>
+        </Slider>
       </div>
-      <div className='seccion-derecha w-1/3 flex flex-col justify-center items-center'>
+      <div className='seccion-derecha w-1/3 flex flex-row justify-center items-center'>
+        <p className='message2'>Tu tienda en cualquier dispositivo</p>
         <LuMonitorSmartphone className='icono-inicio' />
       </div>
     </div>
   </div>
 </>
-
   );
 };
